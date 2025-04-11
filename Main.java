fbdfe9a6aa079877c11d2cdd98e3883e704e3885
powero4ek <124286600+powero4ek@users.noreply.github.com>
2025-04-11 08:10:26 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        File myFile = new File("test.txt");
         String str = in.nextLine();
         Pattern p = Pattern.compile( "(?<!\\S)" + "((25[0-5]|2[0-4]\\d|1\\d\\d|\\d?\\d)\\.){3}" + "(25[0-5]|2[0-4]\\d|1\\d\\d|\\d?\\d)" + "(?!\\S)");
         Matcher m = p.matcher(str);
