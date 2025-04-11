f487d67e9c426ed56c33765f939afb22b463a4cf
powero4ek <124286600+powero4ek@users.noreply.github.com>
2025-04-11 08:09:17 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,20 @@
+import java.io.File;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner in = new Scanner(System.in);
-       String str = in.nextLine();
-       Pattern p = Pattern.compile( "(?<!\\S)" + "((25[0-5]|2[0-4]\\d|1\\d\\d|\\d?\\d)\\.){3}" + "(25[0-5]|2[0-4]\\d|1\\d\\d|\\d?\\d)" + "(?!\\S)");
-       Matcher m = p.matcher(str);
+        Scanner in = new Scanner(System.in);
+        File myFile = new File("test.txt");
+        String str = in.nextLine();
+        Pattern p = Pattern.compile( "(?<!\\S)" + "((25[0-5]|2[0-4]\\d|1\\d\\d|\\d?\\d)\\.){3}" + "(25[0-5]|2[0-4]\\d|1\\d\\d|\\d?\\d)" + "(?!\\S)");
+        Matcher m = p.matcher(str);
 
-       while (m.find()){
-           System.out.println(m.group());
-       }
+        if (m.find()){
+            System.out.println(m.group());
+        }
+        else
+            System.out.println("Не найдено");
     }
 }
